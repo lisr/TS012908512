3f60ceefd1e1b424ce079f63d389ade7ff76cc6f
lisr <lsrlibj@cn.ibm.com>
2023-08-11 09:57:31 +0800

    add firebase messaging

--- App.js
+++ App.js
@@ -80,7 +80,7 @@ const App: () => Node = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Firstly">Hello 2</Section>
+          <Section title="Firstly">Hello 3</Section>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
