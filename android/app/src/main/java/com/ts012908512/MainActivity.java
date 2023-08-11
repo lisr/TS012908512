f11caa92ae7e8327baae89487acc7d61fffc7d6a
lisr <lsrlibj@cn.ibm.com>
2023-08-11 08:28:26 +0800

    init commit

---
+++ android/app/src/main/java/com/ts012908512/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ts012908512;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "TS012908512";
+  }
+}
